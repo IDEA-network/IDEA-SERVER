2a039ddc523fa4c53d3ae57ba746a495657f190c
{cocoide} <cocoide@gmail.com>
2023-07-04 02:04:53 +0900

    config: CORS

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"os"
+
 	"github.com/IDEA/SERVER/conf"
 	"github.com/IDEA/SERVER/pkg/gateway"
 	"github.com/IDEA/SERVER/pkg/handler"
 	"github.com/IDEA/SERVER/pkg/service"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
 	conf.NewEnv()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{os.Getenv("FRONTNED_URL")},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowCredentials: true,
+	}))
 	dg := gateway.NewDiscordGateway()
 	ns := service.NewNotifyService(dg)
 	h := handler.NewHandler(ns)
